ef746064e4908554c033712d0ecccaa7263eb5eb
zhenghu <zhenghu@xiaoshouyi.com>
2017-05-16 09:49:05 +0800

    default my-project-root

--- args.el
+++ args.el
@@ -1,4 +1,4 @@
-(setq my-project-root (file-name-as-directory "~/document/git/private/"))
+(setq my-project-root (file-name-as-directory "~/git/private/"))
 
 ; 用于默认task，定位于
 (defvar my/organization-task-id "f92b1f4a4311f2a93703b883820863e5")
